98566e5a9bc390af3b90af4e92fb4e521b136bac
Ryan Gregory <ryan.middleFiddle@gmail.com>
2023-09-21 16:51:42 -0400

    centered itemsw

--- app/page.tsx
+++ app/page.tsx
@@ -9,12 +9,14 @@ import TheKnotBadge from '../public/VendorBadge_CouplesLoveUs.png'
 export default function Home() {
 
   return (
-    <main className="flex w-100 flex-col items-center px-16 py-4 ">
-      <div>
+    <main className="w-100 px-16 py-4 ">
+      <div className="flex flex-col items-center">
         <h3 className="text-center text-xl italic text-violet-200">{homepageCopy.header}</h3>
-        <Image src={FeaturePhoto} width={1100} height={734} alt='A photo of Tyler in a beautiful blue dress and golden blonde hair alongside her stunning Black Lyon and Healy Model 30 harp.' />
-        <p>{homepageCopy.body}</p>
-        <div className="flex justify-around p-8">
+        <div className="">
+          <Image src={FeaturePhoto} width={1100} height={734} alt='A photo of Tyler in a beautiful blue dress and golden blonde hair alongside her stunning Black Lyon and Healy Model 30 harp.' />
+        </div>
+        <p className="w-2/3">{homepageCopy.body}</p>
+        <div className="w-1/2 flex justify-around p-8">
           <Image src={WeddingWireBadge} width={177} height={177} alt='This is a badge from weddingwire.com recognizing Tyler as a top rated wedding vendor.' />
           <Image src={TheKnotBadge} width={177} height={177} alt='This is a badge from theknot.com recognizing Tyler as a top rated wedding vendor.' />
         </div>
