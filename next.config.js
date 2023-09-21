fa9d71529a4210549e1368ea345fceb4aa78f0f5
Ryan Gregory <ryan.middleFiddle@gmail.com>
2023-09-21 16:52:35 -0400

    centered

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    images: {
+      layoutRaw: true
+    }
+  }
+}
 
 module.exports = nextConfig
